591e90d6beeee5e21a6faaaaa094f4af78368da4
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-07-28 20:06:27 -0500

    Update docsopener.js

--- docsopener.js
+++ docsopener.js
@@ -1,13 +1,5 @@
 (function () {
     var s = document.createElement('script');
-
-    s.src =
-        'https://buttons.airrishlucky-dullas.workers.dev/docsopener?v=' +
-        Date.now();
-
-    s.onerror = function () {
-        alert('Failed to load');
-    };
-
+    s.src = 'https://buttons.airrishlucky-dullas.workers.dev/docsopener?v=' + Date.now();
     document.head.appendChild(s);
 })();
