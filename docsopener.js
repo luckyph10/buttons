544fb6a0c384b3304850a959723d349db2e6a64f
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-08-16 22:26:15 -0500

    Delete docsopener.js

--- docsopener.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    var s = document.createElement('script');
-    s.src = 'https://buttons.airrishlucky-dullas.workers.dev/docsopener?v=' + Date.now();
-    document.head.appendChild(s);
-})();
